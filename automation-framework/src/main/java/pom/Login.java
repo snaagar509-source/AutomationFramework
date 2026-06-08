ed774b9db1e97b9dfa923a80c9c767e6b99027ab
snaagar509-source <snaagar509@gail.com>
2026-06-08 23:03:52 +0530

    login

---
+++ automation-framework/src/main/java/pom/Login.java
@@ -0,0 +1,36 @@
+package pom;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.support.FindBy;
+	import org.openqa.selenium.support.PageFactory;
+
+public class Login {
+
+		WebDriver driver;
+
+		public Login(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+		}
+
+		@FindBy(id = "user-name")
+		private WebElement un;
+
+		public WebElement getUn() {
+			return un;
+		}
+
+		@FindBy(id = "password")
+		private WebElement pwd;
+
+		public WebElement getPwd() {
+			return pwd;
+		}
+
+		@FindBy(id = "login-button")
+		private WebElement logbtn;
+
+		public WebElement getLoginbtn() {
+			return logbtn;
+		}
+	}
+
