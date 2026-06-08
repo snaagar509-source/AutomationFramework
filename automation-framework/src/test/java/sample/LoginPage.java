7af722aec2d1b5049311d9ae9018430e61ff6e77
LENOVO <LENOVO@DESKTOP-OH8204H>
2026-06-08 22:59:30 +0530

    login page and pom dependencies

--- automation-framework/src/test/java/sample/LoginPage.java
+++ automation-framework/src/test/java/sample/LoginPage.java
@@ -1,10 +1,36 @@
 package sample;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
 
-	public static void main(String[] args) {
-		System.out.println("This is Login page");
+	WebDriver driver;
 
+	public LoginPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
 	}
 
+	@FindBy(id = "user-name")
+	private WebElement un;
+
+	public WebElement getUn() {
+		return un;
+	}
+
+	@FindBy(id = "password")
+	private WebElement pwd;
+
+	public WebElement getPwd() {
+		return pwd;
+	}
+
+	@FindBy(id = "login-button")
+	private WebElement logbtn;
+
+	public WebElement getLoginbtn() {
+		return logbtn;
+	}
 }
